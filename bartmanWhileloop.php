080e2d3f705fa387c46ba72187b37266c2f47d0b
salv236 <salvatoresaia@gmail.com>
2017-05-10 12:45:52 +0200

    added loop files foreach and whil in addition to updates to the web developer array file

---
+++ bartmanWhileloop.php
@@ -0,0 +1,14 @@
+<?php
+
+/*You are Bart Simpson and you have been punished: you have to copy 100 times "I do not have to watch flies fly when I learn the PHP. ". 
+Before, we had to take his trouble in patience and it took a lot of work ... Now, with PHP, we will do this in the blink of an eye*/
+
+$lines = 1;
+
+while($lines <=100){
+	echo $lines .' I do not have to watch flies fly when I learn the PHP. <br>';
+	$lines++; // increment from 1 until 100
+}
+
+
+?>
