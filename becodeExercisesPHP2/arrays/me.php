8aa498a0cf6b79ead4ae6e1a52cf380239ae03d2
salv236 <salvatoresaia@gmail.com>
2017-08-21 00:10:46 +0200

    inserted new array php files

---
+++ becodeExercisesPHP2/arrays/me.php
@@ -0,0 +1,13 @@
+<?php
+
+$me = array('firstname' => 'salvatore', 
+	'lastname' => 'saia', 
+	'age' => 39, 
+	'status' => 'single',  
+	'profession' => 'web developer', 
+	'married' => false);
+
+print_r($me);
+echo '<br>';
+
+echo $me['status'];
