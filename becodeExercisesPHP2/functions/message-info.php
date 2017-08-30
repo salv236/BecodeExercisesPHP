8f5f5ecdbf10f8f36f939efa5837d8a12aa7b8e1
salv236 <salvatoresaia@gmail.com>
2017-08-30 09:35:48 +0200

    start of function display html message

---
+++ becodeExercisesPHP2/functions/message-info.php
@@ -0,0 +1,33 @@
+<?php
+
+/*Creates a function allowing you to manage messages to the user with two arguments: the first argument is the message, the second argument specifies an attribute of html class that will be used by the CSS (eg "info", " Error "," warning "). Thanks to this function, we can write in php:
+Echo feedback ( " incorrect e-mail address " , " warning " );
+
+Which will cause this injection of html:
+
+<div  class = "warning"> Invalid email address. </div>*/
+
+function displayHtml($message, $attribute){
+	$html = '<div class = "warning"> Invalid email address. </div>';
+	return $html;
+
+	//$alert = $message  + $attribute;
+
+   }
+
+ displayHtml();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php echo displayHtml();?>
+	
+</body>
+</html>
